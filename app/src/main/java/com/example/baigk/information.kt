675e311231e2ace7acd10fdb275def70b0fcde57
ticchien1 <dchuong202@gmail.com>
2022-03-15 09:14:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/baigk/information.kt
@@ -0,0 +1,11 @@
+package com.example.baigk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class information : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_information)
+    }
+}
